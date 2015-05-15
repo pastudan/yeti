4504d959e53b650725e9385d476be9ff945e512a
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2015-05-15 01:05:53 -0700

    WIP: Decode all the order types

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	//	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/cactus/go-statsd-client/statsd"
 	"github.com/gorilla/websocket"
+	//"github.com/sdming/goh"
+	"io"
 	"log"
 	"net/http"
 )
@@ -21,26 +25,41 @@ type OrderMessage struct {
 	CoinbaseMessage
 	Price    string `json:"price"`
 	Sequence int    `json:"sequence"`
+	Time     string `json:"time"`
+	OrderID  string `json:"order_id"`
+	Side     string `json:"side"`
 }
 
 type ReceivedOrderMessage struct {
 	OrderMessage
+	Size string `json:"size"`
 }
 
 type OpenOrderMessage struct {
 	OrderMessage
+	RemainingSize string `json:"remaining_size"`
 }
 
 type DoneOrderMessage struct {
 	OrderMessage
-}
-
-type MatchOrderMessage struct {
-	OrderMessage
+	Reason        string `json:"reason"`
+	RemainingSize string `json:"remaining_size"`
 }
 
 type ChangeOrderMessage struct {
 	OrderMessage
+	NewSize string `json:"new_size"`
+	OldSize string `json:"old_size"`
+}
+
+type MatchOrderMessage struct {
+	CoinbaseMessage
+	Price        string `json:"price"`
+	Sequence     int    `json:"sequence"`
+	Side         string `json:"side"`
+	MakerOrderID string `json:"maker_order_id"`
+	TakerOrderID string `json:"taker_order_id"`
+	TradeID      int    `json:"trade_id"`
 }
 
 type ErrorMessage struct {
@@ -49,6 +68,21 @@ type ErrorMessage struct {
 }
 
 func main() {
+	log.Printf("Connecting to HBase")
+
+	/*
+		hbaseclient, err := goh.NewTcpClient("127.0.0.1:9090", goh.TBinaryProtocol, false)
+		if err != nil {
+			log.Fatalf("Error connecting to HBase: %s", err.Error())
+		}
+
+		if err = hbaseclient.Open(); err != nil {
+			log.Fatalf("Error opening client to HBase: %s", err.Error())
+		}
+
+		defer hbaseclient.Close()
+	*/
+
 	log.Print("Connecting to the Coinbase Exchange real-time websocket feed...")
 
 	statsdclient, err := statsd.NewClient("statsd.jacobgreenleaf.com:8125", "coinbase.")
@@ -76,15 +110,46 @@ func main() {
 	log.Printf("Subscribed. Reading...")
 
 	for {
-		coinbase_msg := &CoinbaseMessage{}
+		var reader io.Reader
 
-		if err := ws.ReadJSON(coinbase_msg); err != nil {
-			log.Printf("Error reading from websocket: %s", err.Error())
+		_, reader, err := ws.NextReader()
+
+		if err != nil {
+			log.Printf("Error getting reader from websocket: %s", err.Error())
 			break
 		}
 
-		fmt.Printf("Received: %s.\n", coinbase_msg.CommandType)
-		statsdclient.Inc(coinbase_msg.CommandType, 1, 1)
+		decoder := json.NewDecoder(reader)
+		for {
+			var raw_order map[string]interface{}
+			decoder.Decode(&raw_order)
+
+			fmt.Printf("Received %s\n", raw_order)
+			/*
+				coinbase_msg := &CoinbaseMessage{}
+
+				var order interface{}
+
+				if "open" == coinbase_msg.CommandType {
+					order = &OpenOrderMessage{}
+				} else if "received" == coinbase_msg.CommandType {
+					order = &ReceivedOrderMessage{}
+				} else if "match" == coinbase_msg.CommandType {
+					order = &MatchOrderMessage{}
+				} else if "change" == coinbase_msg.CommandType {
+					order = &ChangeOrderMessage{}
+				} else if "done" == coinbase_msg.CommandType {
+					order = &DoneOrderMessage{}
+				} else if "error" == coinbase_msg.CommandType {
+					order = &ErrorMessage{}
+				} else {
+					fmt.Printf("Unknown order type %s", coinbase_msg.CommandType)
+				}
+
+				fmt.Printf("Received: %s.\n", order)
+			*/
+			statsdclient.Inc("", 1, 1)
+		}
 	}
 
 	log.Printf("Exiting...")
