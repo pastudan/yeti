0c074509a2814cf468b42ad03cbeb69dc13f99af
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2015-05-26 06:20:01 -0700

    Main basic loop re-implement

--- yeti/main.go
+++ yeti/main.go
@@ -1,35 +1,86 @@
 package main
 
 import (
+	"bitbucket.org/jacobgreenleaf/yeti/book"
+	"bitbucket.org/jacobgreenleaf/yeti/coinbase"
 	//"container/list"
-	//"encoding/json"
+	"encoding/json"
 	//"fmt"
 	//"github.com/cactus/go-statsd-client/statsd"
-	//"github.com/gorilla/websocket"
-	//"io"
+	"github.com/gorilla/websocket"
+	"io"
 	"log"
-	//"net/http"
+	"net/http"
 )
 
 func main() {
-	/*
-		log.Printf("Connecting to HBase")
+	var err error
 
-		hbaseclient, err := goh.NewTcpClient("hbase:9090", goh.TBinaryProtocol, false)
+	log.Printf("Connecting to Coinbase Exchange real-time API...")
+
+	coinbase_url_raw := "wss://ws-feed.exchange.coinbase.com"
+	coinbase_headers := http.Header{}
+	coinbase_headers.Set("Origin", "http://www.jacobgreenleaf.com")
+	coinbase_headers.Set("User-Agent", "Yeti <jacob@jacobgreenleaf.com>")
+	ws_dialer := websocket.Dialer{}
+	ws, _, err := ws_dialer.Dial(coinbase_url_raw, coinbase_headers)
+
+	if err != nil {
+		log.Fatalf("Error upgrading coinbase exchange feed connection to WebSocket: %s", err.Error())
+	}
+
+	log.Printf("Connected. Subscribing to BTC-USD...")
+
+	ws.WriteMessage(websocket.TextMessage, []byte(`
+		{
+			"type": "subscribe",
+			"product_id": "BTC-USD"
+		}
+	`))
+
+	log.Printf("Synchronizing order book...")
+
+	orderBook := book.NewInMemoryOrderBook()
+
+	for {
+		var reader io.Reader
+		_, reader, err := ws.NextReader()
+
 		if err != nil {
-			log.Fatalf("Error connecting to HBase: %s", err.Error())
+			log.Printf("Error getting next reader from websocket: %s", err.Error())
+			continue
 		}
 
-		if err = hbaseclient.Open(); err != nil {
-			log.Fatalf("Error opening client to HBase: %s", err.Error())
+		decoder := json.NewDecoder(reader)
+
+		for {
+			var rawOrder interface{}
+			decoder.Decode(&rawOrder)
+
+			if rawOrder == nil {
+				break
+			}
+
+			rawBytes, _ := json.Marshal(rawOrder)
+
+			cmds := coinbase.Decode(rawBytes)
+
+			if cmds == nil || len(cmds) == 0 {
+				break
+			}
+
+			log.Printf("%d commands decoded. Applying...", len(cmds))
+
+			for _, cmd := range cmds {
+				err = cmd.Apply(orderBook)
+				if err != nil {
+					log.Printf("Failed to apply order book command: %s", err.Error())
+				}
+			}
+
+			log.Printf("Done applying commands.")
 		}
-
-		defer hbaseclient.Close()
-	*/
-
-	/*
-		statsdclient, err := statsd.NewClient("statsd.jacobgreenleaf.com:8125", "coinbasebeta.")
-	*/
+	}
 
 	log.Printf("Exiting...")
 }
